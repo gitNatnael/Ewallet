f56b4bcf4837ddfd9d244f2c5ad2171095b5e627
Natnael Lijato <natnael.lijato@sll.se>
2021-01-05 14:18:24 +0100

    express

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const serveStatic = require("serve-static");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 5000;
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 app.listen(port, () => {
